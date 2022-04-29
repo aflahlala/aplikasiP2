2db0c46d0cd03a7812cb44c4bc25f04ad91b7993
Bimo S <bimo.s@maxxitani.id>
2022-04-30 02:22:05 +0700

    progress slicing

--- application/views/files_preview/lptp.php
+++ application/views/files_preview/lptp.php
@@ -2,16 +2,16 @@
     <head></head>
     <body>
         <div class="container">
-            <table>
+            <table border="1" >
             <tr>
                 <td>
-                    <img src="<?php echo base_url("assets/images/kop_surat.png")?>" width="170" height="150" >
+                    <img src="<?php echo base_url("assets/images/kop_surat.png")?>" width="100" height="80" >
                 </td>
                 <td style="font-family: Arial, Helvetica, sans-serif;text-align: center;">
-                    <h2>KEMENTERIAN KEUANGAN REPUBLIK INDONESIA</h2>
-                    <h3 style="margin-top: -20px;">DIREKTORAT JENDERAL BEA DAN CUKAI</h3>
-                    <h2 style="margin-top: -20px;">KANTOR WILAYAH DIREKTORAT JENDERAL BEA DAN CUKAI</h2>
-                    <h2 style="margin-top: -20px;">JAWA TIMUR II</h2>
+                    <h3>KEMENTERIAN KEUANGAN REPUBLIK INDONESIA</h3>
+                    <h4 style="margin-top: -20px;">DIREKTORAT JENDERAL BEA DAN CUKAI</h4>
+                    <h3 style="margin-top: -20px;">KANTOR WILAYAH DIREKTORAT JENDERAL BEA DAN CUKAI</h3>
+                    <h3 style="margin-top: -20px;">JAWA TIMUR II</h3>
                     <h6 style="margin-top: -20px;">JALAN. RADEN INTAN NO. 3 MALANG 65125</h6>
                     <h6 style="margin-top: -25px;">TELEPON ; 0341 - 410740 FAKSIMILE ; 0341 – 410739 SITUS www.beacukai.go.id</h6>
                     <h6 style="margin-top: -25px;">PUSAT KONTAK LAYANAN : 1501025 SURAT ELEKTRONIK : info@customs.go.id</h6>
@@ -25,13 +25,70 @@
                 </td>
             </tr>
         </table>
-        <table>
+        <br>
+        <table border="1">
             <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
+                <td style="width: 40px;">1.</td>
+                <td style="width: 120px;">Surat Perintah Nomor</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">PRIN-07/WBC.12/2022</td>
+                <td style="width: 220px;">Tanggal</td>
+                <td style="width: 60px;">:</td>
+                <td>11 Januari 2022</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;">2.</td>
+                <td style="width: 120px;">Locus</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">Warehouse PJT JNE Express, Jalan Tenaga Baru I No. 5, Blimbing, Kota Malang </td>
+                <td style="width: 220px;">Tempus <br> Jam</td>
+                <td style="width: 110px;">: <br> :</td>
+                <td>11 Januari 2022 <br> 16.00 WIB</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;">3.</td>
+                <td style="width: 120px;">Kategori dan Uraian Penindakan</td>
+                <td style="width: 40px;">:</td>
+                <td colspan="4">Penindakan BKC MMEA berupa pemeriksaan dan penegahan </td>
+            </tr>
+            <tr>
+                <td colspan="7"><p style="margin-left: 30px;">A. Sarana Pengangkut</p></td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Jenis</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">………….....……………..</td>
+                <td style="width: 220px;">No. Pol/Voy/Flight</td>
+                <td style="width: 50px;">:</td>
+                <td>…………....</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Nomor Petikemas</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">………….....……………..</td>
+                <td style="width: 220px;">Ukuran</td>
+                <td style="width: 50px;">:</td>
+                <td>…………....</td>
+            </tr>
+            <tr>
+                <td colspan="7"><p style="margin-left: 30px;">B. Barang</p></td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Komoditi/Jenis</td>
+                <td style="width: 40px;">:</td>
+                <td colspan="5">BKC MMEA</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Jumlah</td>
+                <td style="width: 40px;">:</td>
+                <td colspan="5">30 Liter, dengan rincian : - BKC MMEA merk ”Arack” 50 botol @600ml</td>
+            </tr>
+            <tr>
+                <td colspan="7"><p style="margin-left: 30px;">C. Bangunan/ Tempat</p></td>
             </tr>
         </table>
         </div>
