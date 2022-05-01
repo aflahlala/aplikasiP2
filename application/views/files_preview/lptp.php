3f92b30e2ebbbcdd1c80ce022cf76e03995968e3
Bimo S <bimo.s@maxxitani.id>
2022-05-01 11:16:19 +0700

    slicing lptp done

--- application/views/files_preview/lptp.php
+++ application/views/files_preview/lptp.php
@@ -1,11 +1,11 @@
 <html>
-    <head></head>
-    <body>
-        <div class="container">
-            <table border="1" >
+
+<body>
+    <div class="container">
+        <table>
             <tr>
                 <td>
-                    <img src="<?php echo base_url("assets/images/kop_surat.png")?>" width="100" height="80" >
+                    <img src="<?php echo base_url("assets/images/kop_surat.png") ?>" width="170" height="150">
                 </td>
                 <td style="font-family: Arial, Helvetica, sans-serif;text-align: center;">
                     <h3>KEMENTERIAN KEUANGAN REPUBLIK INDONESIA</h3>
@@ -18,7 +18,7 @@
                 </td>
             </tr>
             <tr>
-                <td colspan="2" style="text-align: center;"> 
+                <td colspan="2" style="text-align: center;">
                     <b><u>LAPORAN PELAKSANAAN TUGAS PENINDAKAN (LPTP)</u></b>
                     <br>
                     <p style="margin-top: 0px; font-size: 17px;">Nomor : LPTP-01WBC.12/2022</p>
@@ -26,14 +26,15 @@
             </tr>
         </table>
         <br>
-        <table border="1">
+
+        <table>
             <tr>
                 <td style="width: 40px;">1.</td>
                 <td style="width: 120px;">Surat Perintah Nomor</td>
                 <td style="width: 40px;">:</td>
                 <td style="width: 220px;">PRIN-07/WBC.12/2022</td>
-                <td style="width: 220px;">Tanggal</td>
-                <td style="width: 60px;">:</td>
+                <td style="width: 120px;">Tanggal</td>
+                <td style="width: 10px;">:</td>
                 <td>11 Januari 2022</td>
             </tr>
             <tr>
@@ -41,8 +42,8 @@
                 <td style="width: 120px;">Locus</td>
                 <td style="width: 40px;">:</td>
                 <td style="width: 220px;">Warehouse PJT JNE Express, Jalan Tenaga Baru I No. 5, Blimbing, Kota Malang </td>
-                <td style="width: 220px;">Tempus <br> Jam</td>
-                <td style="width: 110px;">: <br> :</td>
+                <td style="width: 120px;">Tempus <br> Jam</td>
+                <td style="width: 10px;">: <br> :</td>
                 <td>11 Januari 2022 <br> 16.00 WIB</td>
             </tr>
             <tr>
@@ -52,15 +53,17 @@
                 <td colspan="4">Penindakan BKC MMEA berupa pemeriksaan dan penegahan </td>
             </tr>
             <tr>
-                <td colspan="7"><p style="margin-left: 30px;">A. Sarana Pengangkut</p></td>
+                <td colspan="7">
+                    <p style="margin-left: 30px;">A. Sarana Pengangkut</p>
+                </td>
             </tr>
             <tr>
                 <td style="width: 40px;"></td>
                 <td style="width: 120px;">Jenis</td>
                 <td style="width: 40px;">:</td>
                 <td style="width: 220px;">………….....……………..</td>
-                <td style="width: 220px;">No. Pol/Voy/Flight</td>
-                <td style="width: 50px;">:</td>
+                <td style="width: 120px;">No. Pol/Voy/Flight</td>
+                <td style="width: 10px;">:</td>
                 <td>…………....</td>
             </tr>
             <tr>
@@ -68,12 +71,14 @@
                 <td style="width: 120px;">Nomor Petikemas</td>
                 <td style="width: 40px;">:</td>
                 <td style="width: 220px;">………….....……………..</td>
-                <td style="width: 220px;">Ukuran</td>
-                <td style="width: 50px;">:</td>
+                <td style="width: 120px;">Ukuran</td>
+                <td style="width: 10px;">:</td>
                 <td>…………....</td>
             </tr>
             <tr>
-                <td colspan="7"><p style="margin-left: 30px;">B. Barang</p></td>
+                <td colspan="7">
+                    <p style="margin-left: 30px;">B. Barang</p>
+                </td>
             </tr>
             <tr>
                 <td style="width: 40px;"></td>
@@ -88,9 +93,154 @@
                 <td colspan="5">30 Liter, dengan rincian : - BKC MMEA merk ”Arack” 50 botol @600ml</td>
             </tr>
             <tr>
-                <td colspan="7"><p style="margin-left: 30px;">C. Bangunan/ Tempat</p></td>
+                <td colspan="7">
+                    <p style="margin-left: 30px;">C. Bangunan/ Tempat</p>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Alamat</td>
+                <td style="width: 40px;">:</td>
+                <td colspan="4">.................................................................. </td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">No Reg Bangunan / NPPBKC/ dll.</td>
+                <td style="width: 40px;">:</td>
+                <td colspan="4">.................................................................. </td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Pemilik / yang menguasai.</td>
+                <td style="width: 40px;">:</td>
+                <td colspan="4">.................................................................. </td>
+            </tr>
+            <tr>
+                <td colspan="7">
+                    <p style="margin-left: 30px;">D. Orang</p>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Nama</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">………….....……………..</td>
+                <td style="width: 120px;">Tanggal Lahir</td>
+                <td style="width: 10px;">:</td>
+                <td>…………....</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Jenis Kelamin</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">………….....……………..</td>
+                <td style="width: 120px;">Identitas</td>
+                <td style="width: 10px;">:</td>
+                <td>…………....</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Alamat</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">………….....……………..</td>
+                <td style="width: 120px;">Kewarganegaraan</td>
+                <td style="width: 10px;">:</td>
+                <td>…………....</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;">4.</td>
+                <td style="width: 120px;">SB Penindakan</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">SBP-01/WBC.12/2022 </td>
+                <td style="width: 120px;">Tanggal</td>
+                <td style="width: 10px;">:</td>
+                <td>11 Januari 2022</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;">5.</td>
+                <td style="width: 120px;">Diberikan kepada </td>
+                <td style="width: 40px;">:</td>
+                <td colspan="5"></td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Nama</td>
+                <td style="width: 40px;">:</td>
+                <td style="width: 220px;">Toni Indra Permana</td>
+                <td style="width: 120px;">Selaku</td>
+                <td style="width: 10px;">:</td>
+                <td>Kuasanya</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Alamat </td>
+                <td style="width: 40px;">:</td>
+                <td colspan="5">Jl. Simpang Sulfat Utara</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;"></td>
+                <td style="width: 120px;">Identitas </td>
+                <td style="width: 40px;">:</td>
+                <td colspan="5">-</td>
+            </tr>
+            <tr>
+                <td style="width: 40px;">6.</td>
+                <td style="width: 120px;">Hal yang terjadi </td>
+                <td style="width: 40px;">:</td>
+                <td colspan="5">
+                    ......................................................................
+                    <br>......................................................................
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 40px;">7.</td>
+                <td style="width: 120px;">Alasan Tidak Dilakukan Penindakan </td>
+                <td style="width: 40px;">:</td>
+                <td colspan="5">
+                    ......................................................................
+                    <br>......................................................................
+                </td>
             </tr>
         </table>
-        </div>
-    </body>
+
+        <br><br>
+        <table>
+            <tr>
+                <td style="width: 440px;">Mengetahui <br> Kepala Seksi Penindakan I</td>
+                <td>Malang, 11 Januari 2022 <br> Pejabat yang melaksanakan penindakan,</td>
+            </tr>
+            <tr>
+                <td style="height: 100px; vertical-align:bottom;">Kartika Juwono Vincensius</td>
+                <td style="vertical-align:bottom;">R. Brian Adli Anggadipa</td>
+            </tr>
+            <tr>
+                <td>NIP. 19740507 199602 1 001</td>
+                <td>NIP. 19950505 201502 1 003</td>
+            </tr>
+            <tr>
+                <td style="height: 100px;"></td>
+                <td style="vertical-align:bottom;">Andriansyah Puji Kurniawan</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>NIP. 19981105 201812 1 001</td>
+            </tr>
+        </table>
+
+        <br><br><br>
+        <table>
+            <tr>
+                <td style="width: 100px;">Catatan</td>
+                <td style="width: 10px;">:</td>
+                <td>............................................................................................................................</td>
+            </tr>
+            <tr>
+                <td>Tembusan</td>
+                <td>:</td>
+                <td>............................................................................................................................</td>
+            </tr>
+        </table>
+    </div>
+</body>
+
 </html>
